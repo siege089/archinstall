2cd3da8b6d556978d1bb1bb129c816c794765cf5
CJ Fravel <cj@cjfravel.dev>
2022-10-11 20:28:37 -0700

    wip

--- archinstall.py
+++ archinstall.py
@@ -18,19 +18,25 @@ def call_script(script, error_message=None):
             raise Exception(error_message)
         raise Exception(exc.stdout)
 
+def present_options(options, message):
+    user_input = ''
+
+    input_message = f"{message}:\n"
+    for index, item in enumerate(options):
+        input_message += f'{index + 1}) {item}\n'
+
+    while user_input not in map(str, range(1, len(options) + 1)):
+        user_input = input(input_message)
+
+    return int(user_input)-1
+
 
 def select_disk():
     command = "fdisk -l 2> /dev/null | awk '/^Disk \//{print substr($2,0,length($2)-1)}'"
     try:
         output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
         disk_options = output.decode().strip().splitlines()
-        user_input = ''
-
-        input_message = "Pick an option:\n"
-        for index, item in enumerate(disk_options):
-            input_message += f'{index + 1}) {item}\n'
-        while user_input.lower() not in disk_options:
-            user_input = input(input_message)
+        return present_options(disk_options, "Select a disk")
 
 
 
