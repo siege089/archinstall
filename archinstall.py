66a4d69ddf95c18a318177f6de87c9b629cfa8f1
CJ Fravel <cj@cjfravel.dev>
2022-10-12 21:28:03 -0700

    add budgie desktop

--- archinstall.py
+++ archinstall.py
@@ -28,12 +28,18 @@ def call_command(command, error_message=None, supress_error=False):
             raise Exception(exc.stdout)
 
 
-def call_script(script, error_message=None, supress_error=False):
-    return call_command(f"scripts/{script}", error_message, supress_error)
+def call_script(script, parameters="", error_message=None, supress_error=False):
+    print(script)
+    return call_command(f"scripts/{script} {parameters}", error_message, supress_error)
 
 
-def chroot_script(script, error_message=None, supress_error=False):
-    return call_command(f"arch-chroot /mnt bash /usr/local/share/archinstall/scripts/{script}", error_message,
+def chroot_script(script, parameters="", error_message=None, supress_error=False, hide_parameters=False):
+    if hide_parameters:
+        print(script)
+    else:
+        print(f"{script} {parameters}")
+    return call_command(f"arch-chroot /mnt bash /usr/local/share/archinstall/scripts/{script} {parameters}",
+                        error_message,
                         supress_error)
 
 
@@ -63,16 +69,22 @@ def select_disk():
         raise Exception(exc.stdout)
 
 
+def get_install_type_str(install_type, server_install_type, desktop_install_type):
+    if install_type == "Desktop":
+        return f"{install_type} - {desktop_install_type}"
+    return f"{install_type} - {server_install_type}"
+
+
 YES_NO = ["Yes", "No"]
 INSTALL_TYPES = ["Server", "Desktop"]
-INSTALL_TYPES_DESC = ["Subtypes Presented Later",
-                      "Alacritty terminal, basic plasma desktop, bluetooth support, Pipewire audio"]
 SERVER_TYPES = ["Hadoop"]
+DESKTOP_TYPES = ["Plasma", "Budgie"]
+DESKTOP_TYPES_DESC = ["https://kde.org/plasma-desktop/", "https://blog.buddiesofbudgie.org"]
 
 full_cmd_arguments = sys.argv
 argument_list = full_cmd_arguments[1:]
-short_options = "m:"
-long_options = ["mirror="]
+short_options = "m:n:d:u:"
+long_options = ["mirror=", "hostname=", "disk=", "user="]
 try:
     arguments, values = getopt.getopt(argument_list, short_options, long_options)
 except getopt.error as err:
@@ -81,16 +93,27 @@ except getopt.error as err:
     sys.exit(2)
 
 mirror = None
+hostname = None
+disk = None
+username = None
 for current_argument, current_value in arguments:
     if current_argument in ("-m", "--mirror"):
         mirror = current_value
+    if current_argument in ("-n", "--hostname"):
+        hostname = current_value
+    if current_argument in ("-d", "--disk"):
+        disk = current_value
+    if current_argument in ("-u", "--user"):
+        username = current_value
 
 call_script("verify_boot_mode.sh",
-            "Not in UEFI Boot Mode https://wiki.archlinux.org/title/installation_guide#Verify_the_boot_mode")
+            error_message="Not in UEFI Boot Mode https://wiki.archlinux.org/title/installation_guide#Verify_the_boot_mode")
 call_script("connect_to_internet.sh")
 call_script("update_system_clock.sh")
-disk = select_disk()
-hostname = input("Enter a hostname:\n")
+if disk is None:
+    disk = select_disk()
+if hostname is None:
+    hostname = input("Enter a hostname:\n")
 cpu_type = get_cpu_type()
 intel = False
 amd = False
@@ -103,37 +126,16 @@ os.system("stty -echo")
 root_password = input("Enter password for root:\n")
 os.system("stty echo")
 
-username = input("Enter a username:\n")
+if username is None:
+    username = input("Enter a username:\n")
 
 os.system("stty -echo")
 user_password = input(f"Enter password for {username}:\n")
 os.system("stty echo")
 
-print("\n")
-print("********************")
-print(f"Hostname: {hostname}")
-prRed(f"Selected Disk: {disk} !!! DISK WILL BE FORMATTED AND ALL DATA ERASED !!!")
-print(f"Username: {username}")
-print("********************")
-if YES_NO[present_options(YES_NO, "Confirm Installation")] == "No":
-    raise Exception("Installation Aborted")
-
-call_script("unmount.sh", supress_error=True)
-# call_script(f"create_partitions.sh {disk}")
-boot = call_script(f"get_boot_partition.sh {disk}")
-root = call_script(f"get_root_partition.sh {disk}")
-call_script(f"mount_file_system.sh {root} {boot}")
-
-if mirror is not None:
-    call_script(f"select_custom_mirror.sh '{mirror}'")
-else:
-    if YES_NO[present_options(YES_NO, "Provide specific mirror?")] == "Yes":
-        mirror = input("Enter mirror root:\n")
-        call_script(f"select_custom_mirror.sh '{mirror}'")
-    else:
-        call_script("select_mirror.sh")
-
-install_type = INSTALL_TYPES[present_options(INSTALL_TYPES, "Select Install type", INSTALL_TYPES_DESC)]
+server_install_type = None
+desktop_install_type = None
+install_type = INSTALL_TYPES[present_options(INSTALL_TYPES, "Select Install type")]
 packages = "base linux linux-headers linux-zen linux-zen-headers linux-firmware nano networkmanager openssh snapper zsh sudo git base-devel"
 cpu = ""
 if amd:
@@ -143,17 +145,46 @@ if intel:
     packages += " intel-ucode"
     cpu = "intel"
 if install_type == "Desktop":
-    packages += " bluez-utils blueman alacritty plasma xorg pipewire"
+    packages += " bluez-utils blueman alacritty xorg pipewire"
+    desktop_install_type = DESKTOP_TYPES[present_options(DESKTOP_TYPES, "Select Desktop Type", DESKTOP_TYPES_DESC)]
+    if desktop_install_type == "Plasma":
+        packages += " plasma"
+    if desktop_install_type == "Budgie":
+        packages += " sddm budgie-desktop"
 
-# call_script(f"pacstrap.sh '{packages}'")
+print("\n")
+print("********************")
+print(f"Hostname: {hostname}")
+print(f"Selected Disk: {disk}")
+print(f"Username: {username}")
+print(f"Install Type: {get_install_type_str(install_type, server_install_type, desktop_install_type)}")
+print("********************")
+prRed("!!! DISK WILL BE FORMATTED AND ALL DATA ERASED !!!")
+
+if YES_NO[present_options(YES_NO, "Confirm Installation")] == "No":
+    raise Exception("Installation Aborted")
+
+call_script("unmount.sh", supress_error=True)
+call_script("create_partitions.sh", disk)
+boot = call_script("get_boot_partition.sh", disk)
+root = call_script("get_root_partition.sh", disk)
+call_script("mount_file_system.sh", f"{root} {boot}")
+
+if mirror is not None:
+    call_script(f"select_custom_mirror.sh '{mirror}'")
+else:
+    call_script("select_mirror.sh")
+
+prRed("pacstrap will take a while!")
+call_script(f"pacstrap.sh '{packages}'")
 call_script("fstab.sh")
 
 call_script("copy_install_scripts.sh")
 chroot_script("set_timezone.sh")
 chroot_script("set_localization.sh")
-chroot_script(f"set_network_configuration.sh {hostname}")
-chroot_script(f"set_root_password.sh {root_password}")
-chroot_script(f"set_boot_loader.sh {cpu} {root}")
+chroot_script("set_network_configuration.sh", hostname)
+chroot_script("set_root_password.sh", parameters=root_password, hide_parameters=True)
+chroot_script("set_boot_loader.sh", f"{cpu} {root}")
 
 systemctl_enables = ["snapper-boot.timer", "snapper-cleanup.timer", "snapper-timeline.timer", "NetworkManager"]
 if install_type == "Desktop":
@@ -161,7 +192,9 @@ if install_type == "Desktop":
 for systemctl_enable in systemctl_enables:
     chroot_script(f"enable_systemctl_item.sh {systemctl_enable}")
 
-chroot_script(f"add_user.sh {username} {user_password}")
+chroot_script("add_user.sh", parameters=f"{username} {user_password}", hide_parameters=True)
 chroot_script("set_zsh_default_shell.sh root")
-chroot_script(f"set_zsh_default_shell.sh {username}")
-call_script(f"copy_postinstall.sh {username}")
+chroot_script("set_zsh_default_shell.sh", username)
+call_script("copy_postinstall.sh", username)
+if mirror is not None:
+    chroot_script(f"select_custom_mirror.sh '{mirror}'")
--- scripts/create_partitions.sh
+++ scripts/create_partitions.sh
@@ -1,24 +1,8 @@
 disk=$1
 
 echo "1.9.1 Example layouts - UEFI with GPT (no swap)"
-partitions=($(fdisk -l 2> /dev/null | awk -v var="$disk" '$1 ~ var { print $1 }'))
-reverse() {
-    # first argument is the array to reverse
-    # second is the output array
-    declare -n arr="$1" rev="$2"
-    for i in "${arr[@]}"
-    do
-        rev=("$i" "${rev[@]}")
-    done
-}
-reverse partitions partitions_rev
-for i in "${partitions_rev[@]}"
-do
-	echo "Deleting partition $i"
-	wipefs -a "$i" >> arch-install.log
-	(echo "d"; echo ""; echo "w") | fdisk $disk >> arch-install.log
-done
-wipefs -a "$disk" >> arch-install.log
+echo "Erase disk"
+sgdisk -Z $disk
 
 echo "Setting up partitions"
 (echo "g"; echo "n"; echo ""; echo ""; echo "+512M"; echo "t"; echo "1"; echo "n"; echo ""; echo ""; echo ""; echo "t";  echo "2"; echo "20"; echo "p"; echo "w") | fdisk $disk >> arch-install.log
